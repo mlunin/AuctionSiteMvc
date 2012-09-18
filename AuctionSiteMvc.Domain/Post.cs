daa4b9c58a81df6c77a198b48f5a9f2027af9e32
MonsterCoder <monstercoder@gmail.com>
2012-09-18 12:49:23 -0500

    fixing ref

---
+++ AuctionSiteMvc.Domain/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AuctionSiteMvc.Domain
+{
+    public class Post
+    {
+
+        public Post(int id, string title, string description, string owner, DateTime createdTime, DateTime endTime)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Owner = owner;
+            CreatedTime = createdTime;
+            EndTime = endTime;
+        }
+
+        public int Id { get; private set; }
+
+        public string Title { get; private set; }
+
+        public string Description { get; private set; }
+
+        public string Owner { get; private set; }
+
+        public DateTime CreatedTime { get; private set; }
+
+        public DateTime EndTime { get; private set; }
+    }
+}
